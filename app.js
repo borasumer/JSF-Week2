fdff99e921d30298966a5871840d25a5947cb270
Bora Sumer <solasguitarra@hotmail.com>
2019-05-29 20:46:40 -0400

    Added dynamic port choosing

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req,res) => {
 app.get('/about',(req,res) => {
     res.send(`I like long walks on the beach.`);
 });
-
-app.listen(4000, () => console.log(`Listening on 4000`));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Listening on ${port}`));
